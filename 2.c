8626a353f208be766b5884483a2265f2a97e83f1
Pooja1601139 <47471961+Pooja1601139@users.noreply.github.com>
2019-02-09 16:24:50 +0530

    Update 2.c

--- 2.c
+++ 2.c
@@ -3,13 +3,24 @@
 int main(void) {
 	int n;
 	scanf("%d",&n);
+	if(n>0)
+	{
 	if(n%2==0)
 	{
 		printf("Even");
 	}
-	else
+	else if(n%2!=0)
 	{
 		printf("Odd");
+	}
+	else
+	{
+		printf("Zero");
+	}
+	}
+	else
+	{
+		printf("invalid");
 	}
 	return 0;
 }
